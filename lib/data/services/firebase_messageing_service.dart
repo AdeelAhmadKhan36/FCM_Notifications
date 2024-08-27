50f652746da8f2aecac37474d00054e16d9ca939
Adeel Ahmad Khan <97823437+AdeelAhmadKhan36@users.noreply.github.com>
2024-08-27 23:32:52 +0500

    Just  cheked pop up working for terminated and background state

--- lib/data/services/firebase_messageing_service.dart
+++ lib/data/services/firebase_messageing_service.dart
@@ -6,61 +6,41 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 // Initialize local notifications plugin
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-FlutterLocalNotificationsPlugin();
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 // Background message handler
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
-  // Handle background messages if needed
+  FirebaseMessagingService._showNotification(message); // Show notification when the app is in the background
 }
 
 class FirebaseMessagingService {
   static final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
-
   // Initialize Firebase Messaging
   static Future<void> initialize() async {
     // Configure local notifications
-    const AndroidInitializationSettings initializationSettingsAndroid =
-    AndroidInitializationSettings('@mipmap/ic_launcher');
-    const InitializationSettings initializationSettings =
-     InitializationSettings(android: initializationSettingsAndroid);
+    const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+    const InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
     await flutterLocalNotificationsPlugin.initialize(initializationSettings);
 
     FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
     // Foreground message handler
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      try{
-        _showNotification(message);
-      }catch(e){
-
-        Utils().toastMessage(e.toString());
-      }
-
+      _showNotification(message);
     });
 
     // App open from terminated state
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      try{_handleMessage(message);}catch(e){
-
-        Utils().toastMessage(e.toString());
-      }
-
+      _handleMessage(message);
     });
 
     // Handle notifications when the app is completely terminated
     _firebaseMessaging.getInitialMessage().then((RemoteMessage? message) {
-      try{
-        if (message != null) {
-          _handleMessage(message);
-        }
-      }catch(e){
-
-        Utils().toastMessage(e.toString());
+      if (message != null) {
+        _handleMessage(message);
       }
-
     });
   }
 
@@ -87,7 +67,12 @@ class FirebaseMessagingService {
     );
   }
 
+  // Handle the notification tap and navigate to specific screen
   static void _handleMessage(RemoteMessage message) {
-
+    final String? screen = message.data['screen'];
+    if (screen != null) {
+      // Navigate to the specific screen
+      Utils().toastMessage("Navigate to $screen");
+    }
   }
 }
